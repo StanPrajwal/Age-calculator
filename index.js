b23070ef086f24c18b1524ef28771304ca72927f
StanPrajwal <prajwal1927@gmail.com>
2022-11-30 13:59:08 +0530

    age calculator using html and css and js

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const submit = document.getElementById("submit")
 const container = document.querySelector(".container")
 submit.addEventListener("click",calculateAge)
 function calculateAge(){
-    let date1 = Math.abs(parseInt(date.value)-30)
-    let month1 = Math.abs(parseInt(month.value)-11)
-    let year1 = Math.abs(parseInt(year.value)-2022)
-    console.log(Math.abs(parseInt(date)-30))
+    let todate = new Date()
+    let date1 = 0
+    let month1 = 0
+    let year1  = 0
+    if(parseInt(date.value) <= 31 && parseInt(date.value) >0){
+        date1 = Math.abs(parseInt(date.value)-todate.getDate())
+    }
+    if(parseInt(month.value) <=12 && parseInt(month.value) >0){
+        month1 = Math.abs(parseInt(month.value)-todate.getMonth())
+    }
+    if(parseInt(year.value).toString().length === 4){
+        year1 = Math.abs(parseInt(year.value)-todate.getFullYear())
+    }
+   
+    
+    console.log(todate.getDate())
 
     let h1 = document.getElementById('output')
     h1.innerHTML = `Your Age is ${year1} years ${month1} months ${date1} days `
